faf94dc6afbb8e66bf27c1b41b173c32a1785eb3
Garykom <garykom@mail.ru>
2025-04-10 12:19:46 +0300

    Обновление

--- test/test_curl.bat
+++ test/test_curl.bat
@@ -1,5 +1,8 @@
 rem curl --request POST http://localhost:3000/forms/libreoffice/convert --form files=@test1.xlsx --form files=@test1.docx --form merge=false -o my.zip
 rem pause
 
-curl --request POST http://localhost:3000/forms/libreoffice/convert --form files=@test1.xlsx --form files=@test1.docx --form merge=true -o my.pdf
+rem curl --request POST http://localhost:3000/forms/libreoffice/convert --form files=@test1.xlsx --form files=@test1.docx --form merge=true -o my.pdf
+rem pause
+
+curl --request POST http://localhost:3000/forms/libreoffice/convert --form files=@test1.xlsx --form merge=true -o test1.pdf
 pause
